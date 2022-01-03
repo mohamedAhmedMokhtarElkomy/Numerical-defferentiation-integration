5c5c93243a2e63964810ad059234473f809782c4
mohamedAhmedMokhtarElkomy <mohamed.mokhtar.elkomy@gmail.com>
2022-01-03 03:51:09 +0200

    clean and optimize part 1

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from sympy import integrate
 
 import sympy as sym
 
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-def getPoints(equ, a, b, n):
 
+def getPoints(equ, a, b, n):
     h = (b - a) / n
 
     points = []
@@ -22,60 +23,139 @@ def getPoints(equ, a, b, n):
         x += h
     return points, h
 
+
 def getPoint(equ, x) -> float:
     return eval(equ)
 
 
+class NumericalIntegration:
+    def __init__(self, a, b, equOrPoints, n=1):
+
+        self.h = ((b - a) / n)
+        self.equation = ""
+        self.points = []
+        self.a = a
+        self.b = b
+        self.n = n
+
+        # if n > 1:
+        #     self.h = ((b.x - a.x) / n)
+        # else:
+        #     self.h = (b.x - a.x)
+
+        # check whether the input is equation or table of points
+        if isinstance(equOrPoints, str):
+            self.equation = equOrPoints
+            try:
+                self.__getPoints()
+            except:
+                self.__error("Wrong equation format")
+        else:
+            self.points = equOrPoints
+
+    # convert equation to points
+    def __getPoints(self):
+
+        x = self.a
+        while x <= self.b:
+            y = eval(self.equation)
+            self.points.append(Point(x, y))
+            x += self.h
+
+            # used for getting single point ( x ) from equation
+
+    def __getPoint(self, x) -> float:
+        return eval(self.equation)
+
+    def __error(self, message):
+        print(f"Error found: {message}")
+
+    def trapezoidal(self) -> float:
+
+        # Value of integration
+        I = 0.0
+        for j in range(1, self.n):
+            I += self.points[j].y
+        I = (self.h / 2) * (self.points[0].y + self.points[-1].y + 2 * I)
+
+        return I
+
+    def simpson(self) -> float:
+
+        I = 0.0
+
+        # j = 1
+        # while True:
+        #     I += self.points[(2 * j) - 2].y
+        #     I += 4 * self.points[(2 * j) - 1].y
+        #     I += self.points[2 * j].y
+        #     I *= (self.h / 3)
+        #     j+=1
+        #     if j > (self.n / 2):
+        #         break
+
+        if self.n == 1:
+            # I = interpolate(points, (a.x + b.x) / 2, len(points))
+            I = (4 * self.__getPoint((self.a + self.b) / 2)) + self.points[0].y + self.points[-1].y
+            I *= (self.h / 6)
+        else:
+            for j in range(1, int(self.n / 2)):
+                I +=  2 * self.points[j * 2].y
+
+            for j in range(1, int(self.n / 2) + 1):
+                I += 4 * self.points[(j * 2) - 1].y
+
+            I += self.points[0].y + self.points[-1].y
+            I *= (self.h / 3)
+
+        return I
+
+    # TODO
+    def integration(self):
+        return
 
 
-
-#using points
-def trapezoidal(points, h) -> float:
-
+def midPoint(points) -> float:
     a = points[0]
     b = points[-1]
 
-    # Value of integration
-    I = 0.0
-
-    for i in range(1,len(points) - 1):
-        I += points[i].y
-    print(I)
-
-    I = (h / 2) * (a.y + b.y + 2 * I)
-
-    return I
-
-def simpsonComposite(points, n:int) -> float:
-
-    I = 0.0
-    a = points[0]
-    b = points[-1]
-
-    h = (b.x - a.x) / n
-    l = n / 2
-    for i in range(1, int(n/2) + 1):
-        I += points[2*i -1].y
-    I = 4 * I
-
-    for i in range(1, int(n / 2) - 1 + 1):
-        I += 2 * points[2 * i].y
-    I += a.y + b.y
-    I = I * (h/3)
-
-    return I
-
-def simpson(points) -> float:
-
-    a = points[0]
-    b = points[-1]
-
-    # I = interpolate(points, (a.x + b.x) / 2, len(points))
-    I = getPoint("x**2", (0 + 2) / 2)
-    I *= 4
-    I += a.y + b.y
+    I = getPoint("x**2", (a.x + b.x) / 2)
     I *= (b.x - a.x)
-    I /= 6
+
+    return I
+
+
+def midPointComposite(points, n: int) # print(simpson(points))
+    # equ1 = "0.2+25*x-200*(x**2)+675*(x**3)-900*(x**4)+400*(x**5)"
+    # p, h = getPoints(equ1, 0, 0.8, 10)
+    # print(trapezoidal(p, h))
+
+    # mid = 2
+    # trap=4
+    # simpson = 2.667
+    # equ2 = "x**2"
+    # p2, h2 = getPoints(equ2, 0, 2, 1)
+    # print(f"midpoint: {midPoint(p2)}")
+    # print(f"trapezoid: {trapezoidal(p2, h2)}")
+    # print(f"simpson: {simpson(p2)}")
+
+    # equ3 = "x**2"
+    # p3, h3 = getPoints(equ3, 2, 6, 4)
+    # print(f"midpoint: {midPointComposite(p3, 4)}")
+    # print(f"simpson: {simpsonComposite(p3, 4)}")
+    # print(f"trapezoid: {trapezoidal(p3, h3)}")
+
+    # x = sym.symbols('x')
+    # print(sym.integrate(sym.cos(x), x))-> float:
+    I = 0.0
+    a = points[0]
+    b = points[-1]
+    h = (b.x - a.x) / n
+
+    for i in range(0, int(n / 2) + 1):
+        I += points[(2 * i)].y
+
+    I *= 2 * h
 
     return I
 
@@ -95,26 +175,15 @@ def interpolate(f: list, xi: int, n: int) -> float:
 
     return result
 
+
 if __name__ == '__main__':
     # points = (Point(0, 0), Point(1, 10), Point(2, 12), Point(3, 14))
 
-    # print(simpson(points))
-    # equ1 = "0.2+25*x-200*(x**2)+675*(x**3)-900*(x**4)+400*(x**5)"
-    # p, h = getPoints(equ1, 0, 0.8, 10)
-    # print(trapezoidal(p, h))
-
-    #mid = 2
-    #trap=4
-    #simpson = 2.667
-    # equ2 = "x**2"
-    # p2, h2 = getPoints(equ2, 0, 2, 1)
-    # print(f"simpson: {simpson(p2)}")
-    # print(f"trapezoid: {trapezoidal(p2, h2)}")
-
-    equ3 = "x**2"
-    p3, h3 = getPoints(equ3, 2, 6, 4)
-    print(f"simpson: {simpsonComposite(p3,4)}")
-    print(f"trapezoid: {trapezoidal(p3, h3)}")
-
-    # x = sym.symbols('x')
-    # print(sym.integrate(sym.cos(x), x))
+    a = 0
+    b = 0.8
+    # equ = "0.2 + 25*x - 200*(x**2) + 675*(x**3) - 900*(x**4) + 400*(x**5)"
+    # function = NumericalIntegration(0, 0.8, equ, 10)
+    equ = "x**2"
+    function = NumericalIntegration(2, 6, equ, 4)
+    print(f"trapezoidal: {function.trapezoidal()}")
+    print(f"simpson: {function.simpson()}")
